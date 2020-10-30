d5cef3292cf9015a9b5d6b5b305053a134333e04
jungjing1221 <73683393+jungjing1221@users.noreply.github.com>
2020-10-30 12:54:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
-<?php
-echo "<h2>PHP is Fun!</h2>";
-echo "Hello world!<br>";
-?>
+<html>
+  <head>
+    <title>ชื่อ:นางสาว ณัฐวลี จำปาสด(จุ๋งจิ๋ง)</title>
+  </head>
+  <body>
+    <h1>My first Heading</h1>
+    <p>My first Paragraph </p>
+    <img src="dog.jpg">
+    <img src="dog2.jpg">
+    <br>
+    <a href="https://playvalorant.com/th-th/">This is Link</a>
+
+    <h2>The unordered list</h2>
+    </ul>
+    <li>Google</li>
+    <li>Yahoo</li>
+    <li>Amazon</li>
+  </ul>
+
+  </body>
+</html>
