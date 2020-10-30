485ffc7033587d67bbac335a45e773d0ff892fde
jungjing1221 <73683393+jungjing1221@users.noreply.github.com>
2020-10-30 13:03:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,14 @@
 <html>
   <head>
-    <title>ชื่อ:นางสาว ณัฐวลี จำปาสด(จุ๋งจิ๋ง)</title>
+    <title>ITF</title>
   </head>
   <body>
-    <h1>My first Heading</h1>
-    <p>My first Paragraph </p>
+    <h1>ชื่อ:นางสาว ณัฐวลี จำปาสด(จุ๋งจิ๋ง)</h1>
     <img src="dog.jpg">
     <img src="dog3.jpg">
     <br>
     <a href="https://playvalorant.com/th-th/">This is Link</a>
-
-    <h2>The unordered list</h2>
-    </ul>
-    <li>Google</li>
-    <li>Yahoo</li>
-    <li>Amazon</li>
-  </ul>
+    <p>เมื่อตอนป.5 มีเพื่อนสนิทเป็นผู้ชายอยู่ 1 คน</p>
 
   </body>
 </html>
